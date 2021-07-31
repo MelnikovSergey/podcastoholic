09a6645f6b7823347eb1c769b3fcd25f8b8233d1
Sergey Melnikov <39922197+MelnikovSergey@users.noreply.github.com>
2021-07-31 23:38:43 +0300

    Update index.php

--- index.php
+++ index.php
@@ -4,18 +4,15 @@
 
 <!DOCTYPE html>
 <html>
-<head>
-  <title>Title</title>
-</head>
-
+<head><title>Title</title></head>
 <body>
 	<div id="container">
 		<div class="rss-subscribe center">
 			<span>You can subscribe to this feed of episodes via <a href="feed.xml">RSS</a></span>
 		</div>
-    <main> 
-      <!-- Content --> 
-    </main>
+		<main> 
+			<!-- Content --> 
+		</main>
 	</div>
 </body>
 </html>
