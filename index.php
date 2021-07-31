32ef11b9f9d319d11f3f280488f134a9a37ff925
Sergey Melnikov <39922197+MelnikovSergey@users.noreply.github.com>
2021-07-31 23:36:50 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,21 @@
 <?php
-echo 'Entry point';
+  echo 'Entry point';
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Title</title>
+</head>
+
+<body>
+	<div id="container">
+		<div class="rss-subscribe center">
+			<span>You can subscribe to this feed of episodes via <a href="feed.xml">RSS</a></span>
+		</div>
+    <main> 
+      <!-- Content --> 
+    </main>
+	</div>
+</body>
+</html>
