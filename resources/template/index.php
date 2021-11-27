6b65797d98edc1fc66970a516597e11dfadf3ebe
Sergey Melnikov <39922197+MelnikovSergey@users.noreply.github.com>
2021-11-27 23:41:16 +0300

    Create index.php

---
+++ resources/template/index.php
@@ -0,0 +1,20 @@
+<?php
+  echo 'Entry point';
+?>
+
+<!DOCTYPE html>
+<html>
+<head><title>Title</title></head>
+<body>
+	<div id="container">
+		<div class="rss-subscribe center">
+			<span>You can subscribe to this feed of episodes via <a href="feed.xml">RSS</a></span>
+		</div>
+		<main> 
+			<!-- Content --> 
+		</main>
+	</div>
+  <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+  <script src='js/script.js'></script>
+</body>
+</html>
