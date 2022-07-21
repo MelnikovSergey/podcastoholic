d5324e740765a19c72ab8ccd43d925e96446d8b8
unknown <sergeyandmelnikov@gmail.com>
2022-07-21 12:58:41 +0300

    added tests, fix bugs

---
+++ prototype/js/main.js
@@ -0,0 +1,59 @@
+(function() {
+   'use strict';
+
+	var tests = {
+		proximity: 'onuserproximity' in window,
+		light: 'ondevicelight' in window,
+		vibration: 'vibrate' in window.navigator,
+		notification: 'Notification' in window
+	};
+
+	var config = {
+		battery: {
+			lowThreshold: 0.15,
+			criticalThreshold: 0.05
+		},
+		vibration: {
+			lowThreshold: [500, 200, 500],
+			criticalThreshold: [1000]
+		},
+		notification: {
+			lowThreshold: {
+				title: 'Battery level: low',
+				message: 'Please charge your device to avoid automatically pausing the sound.'
+			},
+			criticalThreshold: {
+				title: 'Battery level: critical',
+				message: 'The sound has been stopped to avoid turning off your device.'
+			}
+		},
+		light: {
+			darkThreshold: 50,
+			normalThreshold: 10000
+		}
+	};
+
+	var audio = document.querySelector(".audioPlayer");
+
+	if (tests.proximity) {
+		window.addEventListener('userproximity', function (event) {
+			if (event.near) audio.paused ? audio.play() : audio.pause();
+		});
+	}
+
+	if (tests.light) {
+		window.addEventListener('devicelight', function(event) {
+
+			var light = Math.round(event.value);
+
+			if (light < config.light.normalThreshold) {
+				document.body.className = 'normal-theme';
+			} else if (light < config.light.darkThreshold) {
+				document.body.className = 'dark-theme';
+			} else {
+				document.body.className = 'light-theme';
+			}
+		});
+	}
+
+}(document));
